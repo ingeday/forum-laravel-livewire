7f4e1c10c60d56d0a518fccfa24c2b8883c16305
Eday Alix <admin@solucionesdeingenio.com>
2024-07-27 21:23:13 -0500

    Botón de preguntas, categorías de hilos

---
+++ resources/views/livewire/show-threads.blade.php
@@ -0,0 +1,26 @@
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex gap-10 py-12">
+    <div class="w-64">
+        <a class="block w-full
+        py-4 mb-10
+        bg-gradient-to-r from-blue-600 to-blue-700
+        hover:to-blue-700
+        text-white/90 font-bold
+        text-xs text-center
+        rounded-md
+        " href="">Preguntar</a>
+        <ul>
+            <li class="my-1">
+                <a href="" class="p-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                    <span class="w-2 h-2 rounded-full" style="background-color:#00aced;"></span>Ejemplo
+                </a>
+            </li>
+            <li class="my-1">
+                <a href="" class="p-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                    <span class="w-2 h-2 rounded-full" style="background-color:#b000a1;"></span>Todos
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="w-full"></div>
+
+</div>
